e7b59910c61a44d9b7cadb21bca86e04591288f1
Jonathon Suggs <jsuggs@murmp.com>
2013-12-19 17:25:05 -0600

    Store data

--- download-stores.php
+++ download-stores.php
@@ -1,8 +1,8 @@
 <?php
 
+$numStores = readline('Stores to download: ');
+$storesImported = 0;
 $stores = file('stores.csv');
-$stores = array('1120studios','stikr4u','prettyinspirations','apeacefulstance');
-$stores = array('1120studios');
 foreach ($stores as $store) {
     $store = trim($store);
     $dir = sprintf('store_data/%s', $store);
@@ -12,6 +12,7 @@ foreach ($stores as $store) {
     if (!mkdir($dir)) {
         throw Exception("could not create directory $dir");
     }
+    echo "Downloading $store\n";
     $storeFile = sprintf('%s/store.json', $dir);
     $storeUrl = sprintf('http://api.bigcartel.com/%s/store.json', $store);
     file_put_contents($storeFile, file_get_contents($storeUrl));
@@ -19,4 +20,9 @@ foreach ($stores as $store) {
     $productsFile = sprintf('%s/products.json', $dir);
     $productsUrl = sprintf('http://api.bigcartel.com/%s/products.json', $store);
     file_put_contents($productsFile, file_get_contents($productsUrl));
+    $storesImported++;
+
+    if ($storesImported == $numStores) {
+        break;
+    }
 }
